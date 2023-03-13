8346c50254c901c24fc8e67f2b0ef1f405482e62
Vahan-23 <vanchrecords@gmail.com>
2023-03-13 20:38:00 +0400

    new style for categories and added text

--- voskerich/src/Components/Content/Category.jsx
+++ voskerich/src/Components/Content/Category.jsx
@@ -26,14 +26,22 @@ export default function Category() {
     return (
          <div >
             <section id='section-1' >
-            <div  className="snakeMargin"> 
-            </div>
             <div className="goldSilver">
-            <p  className='categories' >Բաժիններ</p>
                 <div className="goldSilverProp">
-                    <a  href='/golds'><img className='categoriesImage' src={gold} alt="gold" /> Ոսկի</a>
-                    <a href=""><img className='categoriesImage' src={silver} alt="silver" /> Արծաթ</a>
-                    <a href=""><img className='categoriesImage' src={accesor} alt="accesor" /> Բիժուտերիա</a>
+                    <div className="gold">
+                        <h2 className='goldText'> <h1>Ոսկի</h1> Նուրբ ոսկյա շղթաներից մինչև սլաքիթ կտորներ, մենք բոլորի համար ինչ-որ բան ունենք: Մեր ընտրանին ներառում է 14K, 18K և 24K ոսկի տարբեր գույներով, ինչպիսիք են դեղին, սպիտակ և վարդագույն ոսկի: Մենք նաև առաջարկում ենք մի շարք շշմեցնող թանկարժեք քարեր և ադամանդներ՝ ձեր տեսքին փայլ հաղորդելու համար:</h2>
+                    <a  href='/golds'><img className='categoriesImage' src={gold} alt="gold" /><p className='more'> Տեսնել ավելին</p></a>
+                    </div>
+                    <div  className="snakeMargin"> </div>
+                    <div className="silver">
+                        <h2 className='goldText'> <h1>Արծաթ</h1>Պարզ արծաթյա շղթաներից մինչև զարդարված հայտարարություններ, մենք ունենք ամեն ճաշակի և առիթի համար հարմար բան: Մեր հավաքածուն ներառում է վզնոցներ, ապարանջաններ, ականջօղեր, մատանիներ և այլն, որոնք պատրաստված են բարձրորակ 925 հարգի արծաթից:</h2>
+                    <a href=""><img className='categoriesImage' src={silver} alt="silver" /><p className='more'> Տեսնել ավելին</p></a>
+                    </div>
+                    <div  className="snakeMargin"> </div>
+                    <div className="bijuterie">
+                        <h2 className='goldText'><h1>Բիժուտերիա</h1>Շքեղ վզնոցներից մինչև էլեգանտ ապարանջաններ, մենք ունենք բիժուտերիական զարդերի լայն տեսականի, որը կատարյալ է յուրաքանչյուր ոճի և բյուջեի համար: Մեր ընտրանին ներառում է մի շարք նյութեր, ինչպիսիք են rhinestones, մարգարիտները, բյուրեղները և այլն, որոնք նախատեսված են ձեր տեսքին փայլ հաղորդելու համար:</h2>
+                    <a href=""><img className='categoriesImage' src={accesor} alt="accesor" /> <p className='more'> Տեսնել ավելին</p></a>
+                    </div>
                 </div>
             </div>
             </section>
